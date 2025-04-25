52cc92217fcd0c00e10a3217035f5439555b0d5c
Percy <xhc_1007@163.com>
2025-04-25 16:52:05 -0500

    Patch import for frontend

--- minidrag/minidrag/__vllm__.py
+++ minidrag/minidrag/__vllm__.py
@@ -18,6 +18,18 @@ from minidrag.attention.layer import (
 )
 from minidrag.model_executor.models.llama import forward as llama_attn_forward
 
+# frontend
+from minidrag.request import WrapperedRequest
+from minidrag.entrypoints.llm import (
+    generate as llm_generate, 
+    _validate_and_add_requests as llm_validate_and_add_requests, 
+    _add_request as llm_add_request,
+)
+from minidrag.engine.llm_engine import add_request as llm_engine_add_request
+from minidrag.engine.processor import process_inputs as llm_engine_process_inputs
+from minidrag.engine.__init__ import EngineCoreRequest as llm_engine_core_request
+
+
 
 # Step 0: Set environment variable for Triton backend
 os.environ["VLLM_ATTENTION_BACKEND"] = "TRITON_ATTN_VLLM_V1" 
@@ -43,6 +55,20 @@ def proc_patch():
     # Step 1.6: Patch llama attention forward function
     import vllm.model_executor.models.llama
     vllm.model_executor.models.llama.LlamaAttention.forward = llama_attn_forward
+
+    # Step 2: We need to patch the frontend of the vllm to send our dynamic requests to the backend (real model executor)
+    import vllm.v1.request
+    vllm.v1.request.Request = WrapperedRequest
+    import vllm.entrypoints.llm
+    vllm.entrypoints.llm.LLM.generate = llm_generate
+    vllm.entrypoints.llm.LLM._validate_and_add_requests = llm_validate_and_add_requests
+    vllm.entrypoints.llm.LLM._add_request = llm_add_request
+    import vllm.v1.engine.llm_engine
+    vllm.v1.engine.llm_engine.LLMEngine.add_request = llm_engine_add_request
+    import vllm.v1.engine.processor
+    vllm.v1.engine.processor.Processor.process_inputs = llm_engine_process_inputs
+    import vllm.v1.engine.__init__
+    vllm.v1.engine.__init__.EngineCoreRequest = llm_engine_core_request
 
 
 proc_patch()
